c007ce102c9d96407bb2709a5d176bbdbae71054
aokitashipro <webmocha2@gmail.com>
2025-05-29 12:48:47 +0900

    bookResourceful

--- laravel_api/routes/api.php
+++ laravel_api/routes/api.php
@@ -3,9 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController; //追記
+use App\Http\Controllers\Api\BookController; //追記
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResource('/products', ProductController::class); //追記
+
+Route::apiResource('/books', BookController::class);
