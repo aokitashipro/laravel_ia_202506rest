08d9fd3e237cbb0d7b6e6dfd0d3ab163458358a3
aokitashipro <webmocha2@gmail.com>
2025-05-30 14:48:06 +0900

    add mail

--- laravel_api/routes/api.php
+++ laravel_api/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController; //追記
 use App\Http\Controllers\Api\BookController; //追記
+use App\Http\Controllers\Api\MailController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +13,5 @@ Route::get('/user', function (Request $request) {
 Route::apiResource('/products', ProductController::class); //追記
 
 Route::apiResource('/books', BookController::class);
+
+Route::get('/mail-test', [ MailController::class, 'index']); //追記
