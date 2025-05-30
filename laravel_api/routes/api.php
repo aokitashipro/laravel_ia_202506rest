228dc78aeb85775cc6b361afd67d7a1c28b366c7
aokitashipro <webmocha2@gmail.com>
2025-05-30 20:45:30 +0900

    add auth

--- laravel_api/routes/api.php
+++ laravel_api/routes/api.php
@@ -5,13 +5,22 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController; //追記
 use App\Http\Controllers\Api\BookController; //追記
 use App\Http\Controllers\Api\MailController;
+use App\Http\Controllers\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);   // ★任意
+Route::post('/login',    [AuthController::class, 'login'])->name('api.login');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('/products', ProductController::class); //追記
+//Route::apiResource('/products', ProductController::class); //追記
 
 Route::apiResource('/books', BookController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/products', ProductController::class); //追記
+});
 
 Route::get('/mail-test', [ MailController::class, 'index']); //追記
